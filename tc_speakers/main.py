83277796b00eab1b91b3239fdf16b310b3b68dd1
h0useofdupree <joel.riekemann@gmail.com>
2024-10-22 13:49:45 +0200

    code improvements

--- tc_speakers/main.py
+++ tc_speakers/main.py
@@ -3,19 +3,20 @@ import tinytuya as tt
 import argparse
 
 
-d_KRK_R = tt.OutletDevice(
-    dev_id="bf2b01655dfe3fb32fupge",
-    address="192.168.0.195",
-    local_key="(T+gxxMiOKAv3Lru",
-    version=3.3,
-)
-
-d_KRK_L = tt.OutletDevice(
-    dev_id="bf1b6602d2742bf93d8izy",
-    address="192.168.0.35",
-    local_key="V)>ArRhvT$1:etdC",
-    version=3.3,
-)
+d_speakers = [
+    tt.OutletDevice(
+        dev_id="bf2b01655dfe3fb32fupge",
+        address="192.168.0.195",
+        local_key="(T+gxxMiOKAv3Lru",
+        version=3.3,
+    ),
+    tt.OutletDevice(
+        dev_id="bf1b6602d2742bf93d8izy",
+        address="192.168.0.35",
+        local_key="V)>ArRhvT$1:etdC",
+        version=3.3,
+    ),
+]
 
 parser = argparse.ArgumentParser(description="Tuya Smart Controller for speakers (WIP)")
 parser.add_argument("-1", "--on", help="Turn on speakers", action="store_true")
@@ -24,21 +25,28 @@ parser.add_argument("--verbose", help="Output Device status", action="store_true
 args = parser.parse_args()
 
 
-if args.verbose:
-    data_KRK_R = d_KRK_R.status()
-    data_KRK_L = d_KRK_L.status()
-    print(f"Status KRK R: {data_KRK_R}")
-    print(f"Status KRK L: {data_KRK_L}")
+def print_status() -> None:
+    for i, device in enumerate(d_speakers, start=1):
+        status = device.status()
+        print(f"Status KRK {chr(64+i)}: {status}")
 
-if args.on:
+
+def set_state(state):
     try:
-        d_KRK_R.turn_on()
-        d_KRK_L.turn_on()
+        for device in d_speakers:
+            if state == 1:
+                device.turn_on()
+            elif state == 0:
+                device.turn_off()
     except Exception as e:
-        raise e
-elif args.off:
-    try:
-        d_KRK_R.turn_off()
-        d_KRK_L.turn_off()
-    except Exception as e:
-        raise e
+        print(f"Error while changing device state: {e}")
+
+
+if __name__ == "__main__":
+    if args.on:
+        set_state(1)
+    elif args.off:
+        set_state(0)
+
+    if args.verbose:
+        print_status()
